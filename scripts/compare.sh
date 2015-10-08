1915238e1746f8e220d26197da8689007613c06e
Trevor Merrifield <trevorm42@gmail.com>
2015-10-07 21:10:54 -0400

    Small correction to compare.sh

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,6 +1,6 @@
 # laziest way in history to check for documentation changes
 wget -nv https://i3wm.org/docs/ipc.html
-diff ipc.html ipc.html.old
 echo === diff ===
-rm ipc.html
+diff ipc.html ipc.html.old
 echo === end of diff ===
+rm ipc.html
