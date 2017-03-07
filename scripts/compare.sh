c217386c2703237612b392a156c1c5bc3d1eb42f
Trevor Merrifield <trevorm42@gmail.com>
2017-03-07 01:29:54 -0500

    Remove compare.sh
    The easiest way to keep up with changes in i3's ipc docs is to check
    the git history: https://github.com/i3/i3/commits/next/docs/ipc
    Remove compare.sh since it's a clumsier way to get the same info.

--- scripts/compare.sh
+++
@@ -1,6 +0,0 @@
-# laziest way in history to check for documentation changes
-wget -nv https://i3wm.org/docs/ipc.html
-echo === diff ===
-diff ipc.html ipc.html.old
-echo === end of diff ===
-rm ipc.html
